839868f8e955d12edbe31e46079d01a3385c06d9
Carlos Ruiz <caruizag@gmail.com>
2019-09-07 20:27:01 -0700

    Format main go to comply with go fmt tool

--- main.go
+++ main.go
@@ -1,47 +1,49 @@
 package main
-import ("fmt"
-	"os"
+
+import (
 	"bufio"
-	"strings"
+	"fmt"
+	"os"
 	"sort"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	reader   := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 	var n, m int
 	//read n
-	if    _, err := fmt.Scan(&n);    err != nil {
+	if _, err := fmt.Scan(&n); err != nil {
 		fmt.Println(err)
 		return
 	}
-	textN , _ := reader.ReadString('\n')
-	textN=strings.TrimSuffix(textN,"\n")
-	if    _, err := fmt.Scan(&m);    err != nil {
+	textN, _ := reader.ReadString('\n')
+	textN = strings.TrimSuffix(textN, "\n")
+	if _, err := fmt.Scan(&m); err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	textM , _ := reader.ReadString('\n')
-	textM=strings.TrimSuffix(textM,"\n")
+	textM, _ := reader.ReadString('\n')
+	textM = strings.TrimSuffix(textM, "\n")
 	sN := strings.Split(textN, " ")
 	sM := strings.Split(textM, " ")
-	mC:=make( map[string]int)
-	for i:=0 ; i<m;i++{
-		mC[sM[i]]++;
+	mC := make(map[string]int)
+	for i := 0; i < m; i++ {
+		mC[sM[i]]++
 	}
-	for i:=0 ; i<n;i++{
-		mC[sN[i]]--;
+	for i := 0; i < n; i++ {
+		mC[sN[i]]--
 	}
 	var keys []int
 	for k, v := range mC {
-		if v!=0 {
-			i,_:=strconv.Atoi(k)
-			keys = append(keys,i )
+		if v != 0 {
+			i, _ := strconv.Atoi(k)
+			keys = append(keys, i)
 		}
 	}
 	sort.Ints(keys)
-	for _,z := range keys {
-		fmt.Printf("%d ",z)
+	for _, z := range keys {
+		fmt.Printf("%d ", z)
 	}
 }
